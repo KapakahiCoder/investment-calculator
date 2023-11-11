e197e2a094a95377f70e6439a034ce32b3f0c5f6
Kapakahi <rubbishman@gmail.com>
2023-11-11 22:05:51 +0900

    Set up state for the 4 inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
 
 function App() {
+  const [initialInvestment, setInitialInvestment] = useState(0);
+  const [annualInvestment, setannualInvestment] = useState(0);
+  const [expectedReturn, setexpectedReturn] = useState(0);
+  const [duration, setduration] = useState(0);
+
+  function initialInvestmentChangeHandler(amount) {
+    setInitialInvestment(amount);
+  }
+
+  function annualInvestmentChangeHandler(amount) {
+    setannualInvestment(amount);
+  }
+  function expectedReturnChangeHandler(amount) {
+    setexpectedReturn(amount);
+  }
+  function durationChangeHandler(amount) {
+    setduration(amount);
+  }
+
   return (
     <>
       <Header />
-      <UserInput />;
+      <UserInput
+        onChangeInitialInvestment={initialInvestmentChangeHandler}
+        onChangeAnnualInvestment={annualInvestmentChangeHandler}
+        onChangeExpectedReturn={expectedReturnChangeHandler}
+        onChangeDuration={durationChangeHandler}
+      />
+      ;
     </>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,40 @@
-export default function UserInput() {
+export default function UserInput(props) {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            onChange={(e) => props.onChangeInitialInvestment(e.target.value)}
+          ></input>
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            onChange={(e) => props.onChangeAnnualInvestment(e.target.value)}
+          ></input>
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            onChange={(e) => props.onChangeExpectedReturn(e.target.value)}
+          ></input>
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            onChange={(e) => props.onChangeDuration(e.target.value)}
+          ></input>
         </p>
       </div>
     </section>
